c5cd3ba7964f18a68ede51af1df1dedc5ba0ffb5
Adarsh Srivastava <Adarsh.Srivastava@belden.com>
2020-06-25 23:39:04 +0530

    First commit

---
+++ src/main/java/com/github/crowdsourcingplatformapi/CrowdsourcingPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.crowdsourcingplatformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdsourcingPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdsourcingPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/crowdsourcingplatformapi/CrowdsourcingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.crowdsourcingplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdsourcingPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
